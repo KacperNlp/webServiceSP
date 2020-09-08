c2cf706228da637f842cca3a8bb7f1317fdb347b
Kacper Nalepa <kacper.nlp@wp.pl>
2020-09-08 21:27:50 +0200

    news filter

--- routes/news.js
+++ routes/news.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const Article = require("../models/News");
 
 /* GET home page. */
-router.get("/", (req, res, next) => {
-  res.render("index", { title: "News" });
+router.get("/", (req, res) => {
+  const search = req.query.search || "";
+  const findArticles = Article.find({
+    title: RegExp(search.trim(), "i"),
+  }).sort({ date: -1 });
+
+  findArticles.exec((err, data) => {
+    if (err) {
+      return res.redirect("/index");
+    }
+    res.render("news", { title: "Aktualności", data, search });
+  });
 });
 
 module.exports = router;
