a4362c503a8859c894dd6f4acba894271bc0f633
Kacper Nalepa <kacper.nlp@wp.pl>
2020-09-08 23:24:24 +0200

    add quiz into website

--- routes/quiz.js
+++ routes/quiz.js
@@ -1,8 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const Quiz = require("../models/Quiz");
 
 router.get("/", (req, res) => {
-  res.render("Quiz", { message: "Witaj w naszym Quizie!" });
+  const show = !req.session.vote;
+  let sum = 0;
+
+  Quiz.find({}, (err, data) => {
+    data.forEach((item) => {
+      sum += item.vote * 1;
+    });
+    res.render("quiz", { title: "Quiz", data, show, sum });
+  });
 });
 
+router.post("/", (req, res) => {
+  const vote = req.body.season;
+
+  Quiz.findOne({ _id: vote }, (err, data) => {
+    data.vote = data.vote + 1;
+    data.save((err) => {
+      req.session.vote = 1;
+      res.redirect("/quiz");
+    });
+  });
+});
 module.exports = router;
